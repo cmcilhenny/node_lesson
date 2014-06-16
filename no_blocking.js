757800a85ae1720f4dddb7b645cba9405ddd0366
courtney mcilhenny <github@courtneymcilhenny>
2014-06-16 08:33:32 -0700

    updating practice for blocking vs non-blocking js.

--- no_blocking.js
+++ no_blocking.js
@@ -2,4 +2,15 @@
 	// Why are they blocking? 
 	// Rewrite them to be non-blocking
 
-	
+
+// example 1. Classic example (ie. basically every example you'll find on the internet)
+	var contents = fs.readFileSync('/etc/hosts');
+	console.log(contents);
+	console.log('Doing something else');
+
+// example 2. An example that looks kinda what we've been doing with Ruby.
+var post = db.query("select * from posts where id = 1");
+doSomethingWithPost(post)
+doSomethingElse();
+
+// 
